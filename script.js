d572a1297c3511bc944e9ffa5989f8650587f85d
cptran89 <baotranphan2808@gmail.com>
2025-03-04 09:18:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function login()
-{
-  let a=username.value
-  let b=password.value
-  if (a == "Ciara" && b == "123")
-  { window.open ("home.html")
-   document.getElementById(compiler).style.display="block";
-  }
-  else document.write("Không hợp lệ")
+function login() {
+    let a = document.getElementById("username").value;
+    let b = document.getElementById("password").value;
+
+    if (a === "Ciara" && b === "123") {
+        window.open("home.html");
+        document.getElementById("compiler").style.display = "block";
+    } else {
+        document.getElementById("error-message").innerText = "Không hợp lệ";
+    }
+}
